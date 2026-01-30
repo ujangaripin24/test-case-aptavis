25712ab98cce10a6c10601c2b0821fd2606d5f9e
ujangaripin24 <ujangarifin06@gmail.com>
2026-01-30 10:21:45 +0700

    fix controller for dahsboard

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Projects;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class DashboardController extends Controller
+{
+    public function index(Request $request)
+    {
+        return Inertia::render('TaskPage/TaskPage', [
+            'projects' => Projects::with(['dependencies', 'tasks.subtasks'])->get()
+        ]);
+    }
+}
